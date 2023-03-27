9f3d0538237190ce49c05743467d1f0a755793f9
Никита <webmaster@priborelektro.ru>
2023-03-27 15:57:33 +0300

    'project_done'

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,45 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Archin</title>
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+<style>
+    /** {*/
+    /*    max-height: 100vh;*/
+    /*}*/
+
+    /*.table-responsive {*/
+    /*    max-height: 75%;*/
+    /*}*/
+
+    .btn-up {
+        position: fixed;
+        background-color: #125ea8;
+        right: 5%;
+        bottom: 0;
+        border-top-left-radius: 8px;
+        border-top-right-radius: 8px;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 60px;
+        height: 50px;
+    }
+
+    .btn-up_hide {
+        display: none;
+    }
+
+    @media (hover: hover) and (pointer: fine) {
+        .btn-up:hover {
+            background-color: #125ee0;
+        }
+    }
+</style>
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,16 @@
+<a href="http://192.168.0.15/dev" style="margin-right: 20px" class="text-decoration-none text-dark"> <h3> ООО "ПРИБОРЭЛЕКТРО" </h3> </a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a id="main" class="nav-link" href="http://192.168.0.15/"> <h3>На главную </h3> </a>
+        </li>
+        @if(request()->ip() == '192.168.0.15')
+            <li class="nav-item">
+                <a id="admin" class="nav-link" href="http://192.168.0.15/parse"> <h3> Админка </h3> </a>
+            </li>
+        @endif
+    </ul>
+</div>
